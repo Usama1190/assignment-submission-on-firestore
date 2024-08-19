7c925fadc1aca57795e7d26899f9f58c811cb7dc
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-19 00:23:37 -0700

    Keep working on Aug 19, 24

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,10 +1,13 @@
-import { auth, signOut, onAuthStateChanged } from "../firebase.js";
+import {db, collection, addDoc , auth, signOut, onAuthStateChanged } from "../firebase.js";
 
 let logout = document.getElementById('logout');
 let assignmentsDashboard = document.getElementById('assignmentsDashboard');
 
 let navigateAssignment = document.getElementById('navigateAssignment');
 let heading = document.getElementById('heading');
+
+let student_name = document.getElementById('student_name');
+let assignment_link = document.getElementById('assignment_link');
 
 let navigateDashboard = document.getElementById('navigateDashboard');
 
@@ -65,14 +68,29 @@ addLogo.addEventListener('click', function() {
     assignmentList.style.display = 'none';
 });
 
-assignmentSubmit.addEventListener('click', function() {
+const addAssignment = async() => {
     assignmentForm.style.display = 'none';
     assignmentList.style.display = 'block';
-
+    
     let newAssignment = document.createElement('div');
-    let newAssignmentContent = document.createTextNode('Hello, I"m New!');
+    let newAssignmentContent = document.createTextNode(`${student_name.value}, and ${assignment_link.value}`);
+    
 
-    newAssignment.appendChild(newAssignmentContent);
-    newAssignment.classList.add('newAssignment');
-    assignmentList.appendChild(newAssignment);
-});
+    if(student_name.value !== '' && assignment_link !== '') {
+        newAssignment.appendChild(newAssignmentContent);
+        newAssignment.classList.add('newAssignment');
+        assignmentList.appendChild(newAssignment);
+
+        try {
+            const docRef = await addDoc(collection(db, "assignments"), {
+              student_name: student_name.value,
+              assignment_link: assignment_link.value,
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+}
+
+assignmentSubmit.addEventListener('click', addAssignment)
--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, signOut, GoogleAuthProvider  , sendPasswordResetEmail  , createUserWithEmailAndPassword, signInWithPopup ,  signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBsmzTbC-rnW1pq7gZZM6EdcxuJsie2nzY",
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth, GoogleAuthProvider  , signOut, sendPasswordResetEmail  ,createUserWithEmailAndPassword, signInWithPopup ,  signInWithEmailAndPassword, onAuthStateChanged };
+export {db, auth, collection , addDoc, GoogleAuthProvider  , signOut, sendPasswordResetEmail  ,createUserWithEmailAndPassword, signInWithPopup ,  signInWithEmailAndPassword, onAuthStateChanged };
