9c6a6c2bc2a6f2edd6c5196664d7790a7427a720
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-22 05:18:16 -0700

    Keep going on Aug 22, 24

--- signup/signup.js
+++ signup/signup.js
@@ -1,56 +1,77 @@
 import { auth, createUserWithEmailAndPassword, onAuthStateChanged } from "../firebase.js";
 
-
 let formField = document.querySelectorAll('form input');
-
 const [ userEmail, userPassword ] = formField;
 
 let signup_btn = document.getElementById('signup_btn');
-
-let loader = document.getElementById('loader');
-
 let signuptext = document.getElementById('signuptext');
-
+let loader = document.getElementById('loader');
 loader.style.display = 'none';
+let confirmPasswrod = document.getElementById('confirmPasswrod');
+
+console.log(confirmPasswrod);
+
+const [,,passwordUser] = formField;
+
+console.log(passwordUser);
+
+
+
+
+
 
 const signUp = () => {
     event.preventDefault();
     signuptext.innerText = '';
     loader.style.display = 'block';
 
-    createUserWithEmailAndPassword(auth, userEmail.value, userPassword.value)
-    .then((userCredential) => {
+    console.log(confirmPasswrod.value, 'confirmPasswrod');
+    console.log(userPassword.value, 'userPassword');
+    
+    
+
+    if(passwordUser.value !== confirmPasswrod.value) {
+        warning.innerText = "Password doesn't match!";
         signuptext.innerText = 'Signup';
         loader.style.display = 'none';
+    }
+    else {
+        createUserWithEmailAndPassword(auth, userEmail.value, userPassword.value)
+        .then((userCredential) => {
+            signuptext.innerText = 'Signup';
+            loader.style.display = 'none';
 
-        const user = userCredential.user;
+            const user = userCredential.user;
 
-        warning.innerText = '';
+            warning.innerText = '';
 
-        Toastify({
-            text: 'Signup Successfully!',
-            duration: 3000
-        }).showToast();
-        
-        // ...
-    })
-    .catch((error) => {
-        signuptext.innerText = 'Signup';
-        loader.style.display = 'none';
+            Toastify({
+                text: 'Signup Successfully!',
+                duration: 3000
+            }).showToast();
+            
+            // ...
+        })
+        .catch((error) => {
+            signuptext.innerText = 'Signup';
+            loader.style.display = 'none';
 
-        const errorCode = error.code;
-        const errorMessage = error.message;
+            const errorCode = error.code;
+            const errorMessage = error.message;
 
-        warning.innerText = 'invalid input field!';
+            warning.innerText = 'invalid input field!';
 
-        Toastify({
-            text: `${errorMessage}`,
-            duration: 3000
-        }).showToast();
-        
-        // ..
-    });
+            Toastify({
+                text: `${errorMessage}`,
+                duration: 3000
+            }).showToast();
+            
+            // ..
+        });
+    }
 }
+
+
 
 signup_btn.addEventListener('click', signUp);
 
