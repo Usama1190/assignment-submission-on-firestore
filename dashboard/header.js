d1a185d273c79f71d8d32ed877387ec271f32d81
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-10-01 19:53:44 -0700

    Keep working on Oct 02, 24

---
+++ dashboard/header.js
@@ -0,0 +1,80 @@
+const dashboard_wrapper = {
+    dashboard_frontpage_wrapper: document.getElementById('dashboard_frontpage_wrapper'),
+    class_assignments_wrapper: document.getElementById('class_assignments_wrapper'),
+    myassignment_wrapper: document.getElementById('myassignment_wrapper'),
+    newassignment_wrapper: document.getElementById('newassignment_wrapper')
+}
+
+const dashboard_header = {
+    navigate_dashboard: document.getElementById('navigate_dashboard'),
+    navigate_classassignment: document.getElementById('navigate_classassignment'),
+    navigate_newassignment: document.getElementById('navigate_newassignment'),
+    navigate_myassignment: document.getElementById('navigate_myassignment'),
+    navigate_newassignment_btn: document.getElementById('navigate_newassignment_btn'),
+    navigate_myassignment_btn: document.getElementById('navigate_myassignment_btn')
+}
+
+dashboard_wrapper.newassignment_wrapper.style.display = 'none';
+dashboard_wrapper.myassignment_wrapper.style.display = 'none';
+dashboard_wrapper.class_assignments_wrapper.style.display = 'none';
+
+
+
+// ============================ Functionalities ============================
+
+
+dashboard_header.navigate_classassignment.addEventListener('click', function() {
+    dashboard_wrapper.dashboard_frontpage_wrapper.style.display = 'none';
+    dashboard_wrapper.class_assignments_wrapper.style.display = 'block';
+    dashboard_wrapper.newassignment_wrapper.style.display = 'none';
+    dashboard_wrapper.myassignment_wrapper.style.display = 'none';
+
+    dashboard_header.navigate_dashboard.classList.remove('navlink_active');
+    dashboard_header.navigate_classassignment.classList.add('navlink_active');
+    dashboard_header.navigate_myassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_newassignment.classList.remove('navlink_active');
+});
+
+
+dashboard_header.navigate_dashboard.addEventListener('click', function() {
+    dashboard_wrapper.dashboard_frontpage_wrapper.style.display = 'flex';
+    dashboard_wrapper.class_assignments_wrapper.style.display = 'none';
+    dashboard_wrapper.newassignment_wrapper.style.display = 'none';
+    dashboard_wrapper.myassignment_wrapper.style.display = 'none';
+
+    dashboard_header.navigate_dashboard.classList.add('navlink_active');
+    dashboard_header.navigate_classassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_myassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_newassignment.classList.remove('navlink_active');
+});
+
+const navigate_myassignment_func = () => {
+    dashboard_wrapper.dashboard_frontpage_wrapper.style.display = 'none';
+    dashboard_wrapper.myassignment_wrapper.style.display = 'block';
+    dashboard_wrapper.class_assignments_wrapper.style.display = 'none';
+    dashboard_wrapper.newassignment_wrapper.style.display = 'none';
+
+    dashboard_header.navigate_dashboard.classList.remove('navlink_active');
+    dashboard_header.navigate_classassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_myassignment.classList.add('navlink_active');
+    dashboard_header.navigate_newassignment.classList.remove('navlink_active');
+}
+
+const navigate_newassignment_func = () => {
+    dashboard_wrapper.dashboard_frontpage_wrapper.style.display = 'none';
+    dashboard_wrapper.myassignment_wrapper.style.display = 'none';
+    dashboard_wrapper.class_assignments_wrapper.style.display = 'none';
+    dashboard_wrapper.newassignment_wrapper.style.display = 'block';
+
+    dashboard_header.navigate_dashboard.classList.remove('navlink_active');
+    dashboard_header.navigate_classassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_myassignment.classList.remove('navlink_active');
+    dashboard_header.navigate_newassignment.classList.add('navlink_active');
+}
+
+
+
+dashboard_header.navigate_myassignment.addEventListener('click', navigate_myassignment_func);
+dashboard_header.navigate_newassignment.addEventListener('click', navigate_newassignment_func);
+dashboard_header.navigate_myassignment_btn.addEventListener('click', navigate_myassignment_func);
+dashboard_header.navigate_newassignment_btn.addEventListener('click', navigate_newassignment_func);
